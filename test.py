395bef76517d81c95d88e897acf03236ea648360
Allen <Yohoo0987@gmail.com>
2020-06-14 12:50:54 +0800

    upload chromedriver

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from pyquery import PyQuery as pq
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from time import sleep
+import os
 
 def get_exchangeRate():
     d = pq("https://rate.bot.com.tw/xrt?Lang=zh-TW").find('table[title="牌告匯率"] tbody');
@@ -42,10 +43,19 @@ def get_data_from_EPSList():
      return {"rank": rank.split(' '), "stockName": stockName, "peRatio":peRatio.split(' '), "stockPrice" : stockPrice.split(' '), "eps_with4years": eps_with4years.split(' ')};
 
 def get_single_data_from_website(id):
-     d = pq("https://statementdog.com/analysis/tpe/" + id); 
-     return  { "EPS" : d.find('.v')[0].text, 
-               "Rate" : d.find('.v')[1].text, 
-               "Net value" : d.find('.v')[2].text ,
-               "YOY" : d.find('.v')[3].text,
-               "EPS" : d.find('.v')[4].text,
-               "ROE" : d.find('.v')[5].text};
+      chrome_options = webdriver.ChromeOptions()
+      chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+      chrome_options.add_argument("--headless")
+      chrome_options.add_argument("--disable-dev-shm-usage")
+      chrome_options.add_argument("--no-sandbox")
+      driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+     # driver = webdriver.Chrome(executable_path="C:\\Users\\teres\\Desktop\\chromedriver.exe") #Use Chrome
+      driver.get("https://statementdog.com/analysis/tpe/"+ id);   
+      sleep(3);
+      return  { "EPS" : driver.find_elements_by_class_name('v')[0].text, 
+               "Rate" : driver.find_elements_by_class_name('v')[1].text, 
+               "Net value" : driver.find_elements_by_class_name('v')[2].text ,
+               "YOY" : driver.find_elements_by_class_name('v')[3].text,
+               "EPS" : driver.find_elements_by_class_name('v')[4].text,
+               "ROE" : driver.find_elements_by_class_name('v')[5].text};
+
