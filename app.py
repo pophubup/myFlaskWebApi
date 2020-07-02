13dcf68d38fd07556d591bbe28a9d214a46ceb91
Allen <Yohoo0987@gmail.com>
2020-07-02 21:19:55 +0800

    update cros

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 import gunicorn
 import test
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/')
+@cross_origin()
 def index():
    return render_template('index.html')
 
