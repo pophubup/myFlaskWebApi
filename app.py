8195e8a678816df18481dec8abb24b36e812acbb
Allen <Yohoo0987@gmail.com>
2020-07-02 21:34:39 +0800

    update cros

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/')
 @cross_origin()
 def index():
-   return render_template('index.html')
+   return render_template('index.html')  
 
 @app.route('/api/resources/exchange', methods=['GET'])  
 def api_all_exchangeRate():
@@ -17,7 +17,7 @@ def api_all_exchangeRate():
 @app.route('/api/resources/test', methods=['GET'])
 def api_all():
     return test.get_data_from_EPSList();
-
+ 
 @app.route('/api/resources/test/<string:stock_name_id>', methods=['GET'])
 def api_single_data(stock_name_id):
     return test.get_single_data_from_website(stock_name_id);
